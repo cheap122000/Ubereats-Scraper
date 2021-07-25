aae736c5f11a7e983c9944a2d47fbbc1f59bf1eb
Kai.Chen <cheap12200@gmail.com>
2021-07-25 19:56:23 +0800

    ubereats scraper

--- scrape_restaurants.py
+++ scrape_restaurants.py
@@ -38,7 +38,7 @@ def getRestaurants(driver: webdriver.Chrome, url: str, city: str):
             print(f"Scrapped failed in category: {category}")
 
 def getRestaurantInfo(driver: webdriver.Chrome, city: str):
-    address = '台北市大安區仁愛路四段345巷4弄'
+    address = '台北市大安區仁愛路四段345巷4弄' # use fake address
     for city in os.listdir('./src/categories'):
         for category in os.listdir(f'./src/categories/{city}'):
             with open(f'./src/categories/{city}/{category}', 'r', encoding='utf8') as f:
